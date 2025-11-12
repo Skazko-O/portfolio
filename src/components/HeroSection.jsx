80f06271352c7f3a2251547cb3445b6e591c9583
Skazko_o <skazko@outlook.com>
2025-11-12 11:21:37 +0200

    Create component Herosection. Added style.

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+
+    return (
+        <>
+            <div className="background">
+                <div className='container'>
+                    <div className="center-section">
+                        <div className="specialisation">
+                            Front-End Developer
+                        </div>
+                        <div className="heading">
+                            <div className="name-heading">
+                                portfolio
+                            </div>
+                            <div className="year">
+                                2025
+                            </div>
+                        </div>
+                    </div>
+                    <div className="fullName">Skazko Oleksandr</div>
+                </div>
+            </div>
+        </>
+    )
+}
